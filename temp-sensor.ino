44c10843c5180a1f70bb376620fc6d54e74dc2dd
Daniel Törbacka <daniel.torbacka@gmail.com>
2019-01-05 10:56:55 +0100

    Added WifiManger, easier to change wifi

--- temp-sensor.ino
+++ temp-sensor.ino
@@ -3,22 +3,27 @@
 #include <DallasTemperature.h>
 #include <config.h>
 
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-
 #include <ESP8266HTTPClient.h>
 
 #include <WiFiClient.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
 #define ONE_WIRE_BUS 14
 ESP8266WiFiMulti WiFiMulti;
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-const char *ssid = "9B4492F9EE";
-const char *password = "Telia-6A2A55";
+
+char static_ip[16] = "10.0.1.59";
+char static_gw[16] = "10.0.1.1";
+char static_sn[16] = "255.255.255.0";
 
 void setup(void) {
+   
     Serial.begin(115200);
     sensors.begin();
 
@@ -28,20 +33,30 @@ void setup(void) {
         delay(1000);
     }
 
-    WiFi.mode(WIFI_STA);
-    WiFiMulti.addAP(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
- 
-    delay(500);
-    Serial.println("Waiting for connection");
- 
-  }
+    WiFiManager wifiManager;
+    
+    //tries to connect to last known settings
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP" with password "password"
+    //and goes into a blocking loop awaiting configuration
+    if (!wifiManager.autoConnect("AutoConnectAP")) {
+        Serial.println("failed to connect, we should reset as see if it connects");
+        delay(3000);
+        ESP.reset();
+        delay(5000);
+     }
+
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+    Serial.println("local ip");
+    Serial.println(WiFi.localIP());
+    
 }
 
 void loop(void) { 
     // Wait for WiFi connection
     if ((WiFiMulti.run() == WL_CONNECTED)) {
-        sendPost("http://192.168.1.242:8090/temperature", getTemperatur());
+        sendPost("http://temp.torbacka.se", getTemperatur());
     } else {
         Serial.printf("[HTTP] Unable to connect\n");
     }
@@ -63,14 +78,14 @@ void sendPost(String url, String data) {
     Serial.print("[HTTP] begin...\n");
     // Start of request
     if (http.begin(client, url)) {
-        Serial.print("[HTTP] GET...\n");
+        Serial.print("[HTTP] POST...\n");
         http.addHeader("Content-Type", "application/json;charset=UTF-8");
         int httpCode = http.POST(data);
   
         // httpCode will be negative on error
         if (httpCode > 0) {
             // HTTP header has been send and Server response header has been handled
-            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+            Serial.printf("[HTTP] POST... code: %d\n", httpCode);
     
             // file found at server
             if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
@@ -78,7 +93,7 @@ void sendPost(String url, String data) {
               Serial.println(payload);
             }
         }else {
-            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+            Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
         }
     } 
     http.end();
