623665b35ed182d13cd4dd27497b313d0fbb09b4
Daniel Törbacka <daniel.torbacka@gmail.com>
2020-06-07 14:49:21 +0200

    Use mqtt and deep sleep

--- temp-sensor.ino
+++ temp-sensor.ino
@@ -1,106 +1,72 @@
-#include <Arduino.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <config.h>
-
-#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
-#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 
 #include <WiFiClient.h>
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <PubSubClient.h>
 
-#define ONE_WIRE_BUS 14
-ESP8266WiFiMulti WiFiMulti;
-
+// Include the libraries we need
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 2
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+DeviceAddress insideThermometer;
 
-char static_ip[16] = "10.0.1.59";
-char static_gw[16] = "10.0.1.1";
-char static_sn[16] = "255.255.255.0";
+const char* ssid     = "x";   
+const char* password = "x";     
+void setup() {
+  sensors.begin();
+  Serial.begin(57600);
 
-void setup(void) {
+  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
+  sensors.setResolution(insideThermometer, 11);
+  delay(10);
+  WiFi.begin(ssid, password);
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
    
-    Serial.begin(115200);
-    sensors.begin();
 
-    for (uint8_t t = 4; t > 0; t--) {
-        Serial.printf("[SETUP] WAIT %d...\n", t);
-        Serial.flush();
-        delay(1000);
-    }
 
-    WiFiManager wifiManager;
-    
-    //tries to connect to last known settings
-    //if it does not connect it starts an access point with the specified name
-    //here  "AutoConnectAP" with password "password"
-    //and goes into a blocking loop awaiting configuration
-    if (!wifiManager.autoConnect("AutoConnectAP")) {
-        Serial.println("failed to connect, we should reset as see if it connects");
-        delay(3000);
-        ESP.reset();
-        delay(5000);
-     }
-
-    //if you get here you have connected to the WiFi
-    Serial.println("connected...yeey :)");
-    Serial.println("local ip");
-    Serial.println(WiFi.localIP());
-    
+  sensors.requestTemperatures(); 
+  String temperature = String(sensors.getTempC(insideThermometer), 3);
+  sendPost("https://temp.torbacka.se", temperature);
+  ESP.deepSleep(36e8);
 }
 
-void loop(void) { 
-    // Wait for WiFi connection
-    if ((WiFiMulti.run() == WL_CONNECTED)) {
-        sendPost("http://temp.torbacka.se", getTemperatur());
-    } else {
-        Serial.printf("[HTTP] Unable to connect\n");
-    }
-
-    sleep(1000);
-}
-
-
-// TODO: Deep sleep to save battery
-void sleep(long time) {
-  delay(time);
-}
-
-// TODO: Should move it out to my own HTTP library for future use
 void sendPost(String url, String data) {
-    WiFiClient client;
-    HTTPClient http;
-
-    Serial.print("[HTTP] begin...\n");
+    WiFiClient wifiClient;
+    PubSubClient client(wifiClient);
+    client.setServer("192.168.86.35", 18831);
     // Start of request
-    if (http.begin(client, url)) {
-        Serial.print("[HTTP] POST...\n");
-        http.addHeader("Content-Type", "application/json;charset=UTF-8");
-        int httpCode = http.POST(data);
-  
-        // httpCode will be negative on error
-        if (httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+    while (!client.connected()) {
+      Serial.println("Connecting to MQTT...");
+      
+      if (client.connect("ESP8266Client", "taco", "x")) {
+      
+        Serial.println("connected");  
+      
+      } else {
+      
+        Serial.print("failed with state ");
+        Serial.print(client.state());
+        delay(2000);
+     
+      }
+    }
+    char buf[40];
+    boolean published = client.publish("temp", data.c_str());
+    sprintf(buf, "PubSubClient status: %d  published: %d", client.state(), published);
+    Serial.println(buf);
+    client.disconnect();
+    while (client.connected()) {
+      delay(50);
+    }
     
-            // file found at server
-            if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-              String payload = http.getString();
-              Serial.println(payload);
-            }
-        }else {
-            Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-    } 
-    http.end();
 }
 
-String getTemperatur() {
-    sensors.requestTemperatures(); 
-    String temperatur = String(sensors.getTempCByIndex(0), 3);
-    return String("{ \"temparatur\": " + temperatur + " }"); 
+void loop() {
+  
 }
